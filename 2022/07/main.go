299003ebd2a1450d937a5da3d2561683e1ec27af
Andrew den Hertog <andrew@denhertog.ca>
2023-12-01 09:00:09 -0500

    22-07
    Signed-off-by: Andrew den Hertog <andrew@denhertog.ca>

---
+++ 2022/07/main.go
@@ -0,0 +1,13 @@
+package main
+
+type file struct {
+	name string
+	path []string
+	size int
+}
+
+func main() {
+	// parse through list of commands creating file objects for each, then create a function
+	// that sorts by path, then start recursing through each to find the appropate dir with
+	// size > that 10000
+}
